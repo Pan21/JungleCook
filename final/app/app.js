68e244d719c1e6d1cf7559aa184bc6df620d2475
Pan21 <49293503+Pan21@users.noreply.github.com>
2020-12-06 17:38:52 -0500

    Add files via upload

---
+++ final/app/app.js
@@ -0,0 +1,82 @@
+
+
+
+function initListeners(){
+    $("nav a ").click(function(e){
+        
+        var btnID = this.id;
+        
+        MODEL.getView(btnID);
+    });
+
+    $("button").click(function(e){
+        
+        var btnID = this.id;
+        
+        MODEL.getView(btnID);
+    });
+
+    $(".navicon").click(function(e){
+        $("nav").toggleClass( "navMobileView");
+        
+        
+    })
+
+
+
+
+}
+
+function viewDetail(){
+    console.log("clicked")
+    var btnID = "detailRecipe";
+    MODEL.getView(btnID);
+
+}
+
+
+
+function initPages(){
+    $.get('views/nav.html', function(nav){
+        $("#nav").html(nav);
+        initListeners();
+    });
+
+    $.get('views/home.html', function(data){
+        $("#content").html(data);
+    });
+}
+
+$(document).ready(function(){
+    initPages();
+}); 
+
+
+function loginAlert(){
+
+    swal({
+        title: "Welcome,",
+        text: "You Have been logged in!",
+        icon: "success",
+        button: "Continue",
+      });
+
+      Logged();
+}
+
+function Logged(){
+
+    if (document.getElementById("loggedIn").style.display === "none") {
+        document.getElementById("loggedIn").style.display = "block";
+      document.getElementById("loggedOut").style.display = "none"
+    } else {
+      document.getElementById("loggedIn").style.display = "none";
+      document.getElementById("loggedOut").style.display = "block";
+    }
+
+    
+    }
+
+
+
+
---
+++ final/model/model.js
@@ -0,0 +1,18 @@
+var MODEL = (function(){
+
+
+    var _getView = function(viewName){
+        $.get(`../views/${viewName}.html`, function(data){
+            $("#content").html(data);
+        });
+    };
+
+
+    return {
+        getView: _getView,
+    }
+})();
+
+
+
+
